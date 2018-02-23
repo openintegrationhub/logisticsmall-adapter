575948bd8dc647a222322a1c97c55acc36e8d211
Khatuna Bregvadze <Khatuna.Bregvadze@logata.com>
2018-02-23 17:45:25 +0100

    alles angepasst

--- src/main/java/io/logmall/actions/CreateShipment.java
+++ src/main/java/io/logmall/actions/CreateShipment.java
@@ -1,12 +1,20 @@
 package io.logmall.actions;
 
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.StringReader;
+import java.util.Scanner;
+
 import javax.json.JsonObject;
 import javax.json.JsonString;
 import javax.xml.bind.JAXBException;
+import javax.xml.bind.Unmarshaller;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import de.fraunhofer.ccl.bo.converter.xml.oagis.BusinessObjectContextResolver;
+import de.fraunhofer.ccl.bo.converter.xml.oagis.XmlFactory;
 import de.fraunhofer.ccl.bo.instancerepository.boundary.rest.api.ShipmentService;
 import de.fraunhofer.ccl.bo.integration.resteasy.ResteasyIntegration;
 import de.fraunhofer.ccl.bo.model.bod.BusinessObjectDocument;
@@ -17,6 +25,7 @@ import io.elastic.api.ExecutionParameters;
 import io.elastic.api.Message;
 import io.elastic.api.Module;
 import io.logmall.bod.ShipmentJsonMapper;
+import io.logmall.res.ResourceResolver;
 
 /**
  * Action to create a Shipment.
@@ -32,6 +41,7 @@ public class CreateShipment implements Module {
      */
     @Override
     public void execute(final ExecutionParameters parameters) {
+    	
         logger.info("Going to create new shipment");
         // incoming message
         final Message message = parameters.getMessage();
@@ -45,23 +55,40 @@ public class CreateShipment implements Module {
         JsonString serverURL = configuration.getJsonString("serverURLd");
         logger.info("App Server URL: " + serverURL.getString());
         
-
+        
+        String changeShipmentXML;
+        Scanner scanner = null;
+        
         try {
-        	ShipmentJsonMapper shipmentJsonMapper = new ShipmentJsonMapper();
-        	ChangeShipment changeShipment = shipmentJsonMapper.fromJson(body);
+        	
+        	InputStream inputStream = ResourceResolver.class.getClassLoader().getResourceAsStream("io/logmall/res/ChangeShipment.xml");
+        	InputStreamReader inputStreamReader = new InputStreamReader(inputStream); 
+        	scanner = new Scanner(inputStreamReader);
+        	changeShipmentXML = scanner.useDelimiter("\\A").next();
+        	XmlFactory xmlFactory = new XmlFactory(new BusinessObjectContextResolver());
+        	Unmarshaller unmarshaller = xmlFactory.createUnmarshaller();
+			ChangeShipment changeShipment = (ChangeShipment) unmarshaller.unmarshal(new StringReader(changeShipmentXML));
+        	
+//            ShipmentJsonMapper shipmentJsonMapper = new ShipmentJsonMapper();
+//        	ChangeShipment changeShipment = shipmentJsonMapper.fromJson(body);
 			ShipmentService shipmentService = ResteasyIntegration.newInstance().createClientProxy(ShipmentService.class, serverURL.getString());
 			logger.info("Got ServerURL " + serverURL.getString());
 			BusinessObjectDocument<Respond, Shipment> response = shipmentService.put(changeShipment);
 			logger.info("Shipment successfully created");
-	        final Message data = new Message.Builder().body(shipmentJsonMapper.fromBOD(response)).build();
+//	        final Message data = new Message.Builder().body(shipmentJsonMapper.fromBOD(response)).build();
 	        logger.info("Emitting data: " + response);
 	        // emitting the message to the platform
-	        parameters.getEventEmitter().emitData(data);
+	       // parameters.getEventEmitter().emitData(data);
 
         } catch (JAXBException e) {
 			logger.error(e.getMessage(),e);
 			throw new IllegalStateException("Exception during API call: " + e.getMessage());
 		}
+        finally{
+        	if(scanner != null) {
+        	scanner.close();
+        	}
+        }
     }
   
 }
