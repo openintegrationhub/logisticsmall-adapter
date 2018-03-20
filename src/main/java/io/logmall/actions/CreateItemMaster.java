e2029df75c908241299e40573de5acba91dd8547
Khatuna Bregvadze <Khatuna.Bregvadze@logata.com>
2018-03-20 16:50:03 +0100

    missing attributes for itemMaster

--- src/main/java/io/logmall/actions/CreateItemMaster.java
+++ src/main/java/io/logmall/actions/CreateItemMaster.java
@@ -1,29 +1,40 @@
 package io.logmall.actions;
 
+
+import java.math.BigDecimal;
+
 import javax.json.JsonObject;
 import javax.json.JsonString;
 import javax.xml.bind.JAXBException;
-
+import org.joda.time.Period;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import de.fraunhofer.ccl.bo.converter.util.MeasureUtil;
+import de.fraunhofer.ccl.bo.converter.util.QuantityUtil;
 import de.fraunhofer.ccl.bo.instancerepository.boundary.rest.api.ItemMasterService;
 import de.fraunhofer.ccl.bo.integration.resteasy.ResteasyIntegration;
 import de.fraunhofer.ccl.bo.model.bod.BusinessObjectDocument;
 import de.fraunhofer.ccl.bo.model.bod.builder.change.CreateOrReplaceBODBuilder;
 import de.fraunhofer.ccl.bo.model.bod.verb.Change;
 import de.fraunhofer.ccl.bo.model.bod.verb.Respond;
+import de.fraunhofer.ccl.bo.model.entity.common.Identifier;
+import de.fraunhofer.ccl.bo.model.entity.common.Measure;
+import de.fraunhofer.ccl.bo.model.entity.common.PredefinedMeasureUnitType;
 import de.fraunhofer.ccl.bo.model.entity.common.QuantityClassification;
 import de.fraunhofer.ccl.bo.model.entity.common.Status;
 import de.fraunhofer.ccl.bo.model.entity.itemmaster.ItemMaster;
-import de.fraunhofer.ccl.bo.model.entity.shipment.Shipment;
 import io.elastic.api.ExecutionParameters;
 import io.elastic.api.Module;
 import io.logmall.Constants;
 import io.logmall.bod.ItemMasterMinimal;
 import io.logmall.mapper.ItemMasterMinimalJsonMapper;
 
+
 public class CreateItemMaster implements Module {
+	public MeasureUtil measureUtil = new MeasureUtil();
+    public QuantityUtil quantityUtil = new QuantityUtil();
+
 	private static final Logger LOGGER = LoggerFactory.getLogger(CreateItemMaster.class);
 
 	/**
@@ -51,13 +62,31 @@ public class CreateItemMaster implements Module {
 			if("1".equals(itemMasterMinimal.getStatusCode())) {
 				status = "active";
 			}
-
+            
 			ItemMaster itemMaster = new ItemMaster();
 			itemMaster.setDisplayIdentifier(itemMasterMinimal.getIdentifier());
 			itemMaster.setBaseQuantityClassification(quantityClassification);
 			itemMaster.setDescription(itemMasterMinimal.getDescription());
 			itemMaster.setStatus(new Status(status));
+
+			itemMaster.setAbcClass(" ");
+			itemMaster.setLeadTimeDuration(new Period());
+			itemMaster.setManufacturerIdentifier(new Identifier(" "));
 			
+			itemMaster.setAverageRunSizeQuantity(this.quantityUtil.newQuantity(BigDecimal.ZERO, "Stk", itemMaster));
+			itemMaster.setOrderQuantity(this.quantityUtil.newQuantity(BigDecimal.ZERO, "Stk", itemMaster));
+			itemMaster.setTargetStock(getMeasure(PredefinedMeasureUnitType.COUNTABLE));
+			itemMaster.setReorderPoint(getMeasure(PredefinedMeasureUnitType.COUNTABLE));
+			itemMaster.setWeight(getMeasure(PredefinedMeasureUnitType.WEIGHT));
+			itemMaster.setLength(getMeasure(PredefinedMeasureUnitType.LENGTH));
+			itemMaster.setWidth(getMeasure(PredefinedMeasureUnitType.LENGTH));
+			itemMaster.setHeight(getMeasure(PredefinedMeasureUnitType.LENGTH));
+			itemMaster.setServiceLevel(getMeasure(PredefinedMeasureUnitType.SERVICE_LEVEL));
+	       
+
+
+			
+	        
             CreateOrReplaceBODBuilder.Builder<ItemMaster> createBODBuilderItemMaster = CreateOrReplaceBODBuilder
                     .newInstance(ItemMaster.class);
             createBODBuilderItemMaster.forCreation();
@@ -83,5 +112,10 @@ public class CreateItemMaster implements Module {
 			throw new IllegalStateException("Exception during API call: " + e.getMessage());
 		}
 	}
-
+	
+	private Measure getMeasure(String type) {
+        String unit = this.measureUtil.getBaseUnit(type);
+        BigDecimal value = BigDecimal.ZERO;
+        return this.measureUtil.newMeasure(value, unit, type);
+    }
 }
--- src/main/java/io/logmall/mapper/ItemMasterMinimalJsonMapper.java
+++ src/main/java/io/logmall/mapper/ItemMasterMinimalJsonMapper.java
@@ -9,6 +9,8 @@ import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
 import javax.xml.bind.Marshaller;
 import javax.xml.bind.Unmarshaller;
+import javax.xml.stream.XMLStreamReader;
+import javax.xml.transform.stream.StreamSource;
 
 import org.eclipse.persistence.jaxb.MarshallerProperties;
 import org.eclipse.persistence.jaxb.UnmarshallerProperties;
@@ -27,14 +29,16 @@ public class ItemMasterMinimalJsonMapper {
 
 	public ItemMasterMinimal fromJson(JsonObject json) throws JAXBException {
 		LOGGER.info("fromJson: " + json);
-		StringReader stringReader = new StringReader(json.toString());
+		
+		StreamSource jsonStream = new StreamSource(new StringReader(json.toString()));
 		try {
 			Unmarshaller unmarshaller = JAXBContext.newInstance(ItemMasterMinimal.class).createUnmarshaller();
 			unmarshaller.setProperty(UnmarshallerProperties.MEDIA_TYPE, MediaType.APPLICATION_JSON);
-			unmarshaller.setProperty(UnmarshallerProperties.JSON_INCLUDE_ROOT, false);
-			return JAXB.unmarshal(stringReader, ItemMasterMinimal.class);
+			unmarshaller.setProperty(UnmarshallerProperties.JSON_INCLUDE_ROOT, true);
+			return unmarshaller.unmarshal (jsonStream, ItemMasterMinimal.class).getValue();
+			//return JAXB.unmarshal(stringReader, ItemMasterMinimal.class);
 		} finally {
-			stringReader.close();
+			// jsonStream.close();
 		}
 	}
 }
