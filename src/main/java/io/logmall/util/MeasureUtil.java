dcf536a7713d34d620fdaa12862d3788e0753409
Khatuna Bregvadze <Khatuna.Bregvadze@logata.com>
2018-08-27 12:11:43 +0200

    Beim Erzeugen von Measure wird Velue mit null gefuelt

--- src/main/java/io/logmall/util/MeasureUtil.java
+++ src/main/java/io/logmall/util/MeasureUtil.java
@@ -12,7 +12,7 @@ public class MeasureUtil {
 		String unit = PredefinedMeasureUnitType.getForType(type).iterator().next().getUnit();
 		
 		Measure measure = new Measure();
-		measure.setValue(null);
+		measure.setValue(new BigDecimal("0.0"));
 		measure.setBaseUnitValue(new BigDecimal("1.0"));
 		measure.setUnitName(unit);
 		measure.setType(type);
